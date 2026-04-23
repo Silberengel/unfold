a155bebd05ef084c16a23c1880ad1965cce666b7
Silberengel <silberengel7@protonmail.com>
2026-04-23 21:34:38 +0200

    bug-fixes

--- src/Twig/Components/UserMenu.php
+++ src/Twig/Components/UserMenu.php
@@ -3,10 +3,15 @@
 namespace App\Twig\Components;
 
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent]
 class UserMenu
 {
     use DefaultActionTrait;
+
+    /** When true, render for the mobile header menu (not fixed in the left column). */
+    #[LiveProp]
+    public bool $inline = false;
 }
